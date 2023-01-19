4490b114513faf417f110d6502eec8fdbe355c60
Suzy Murray <jsmurray86@gmail.com>
2023-01-18 18:10:32 -0800

    highscores

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,10 +1,14 @@
 
 
 const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('#highScores')) || []
 
-highScoresList.innerHTML =
-highScores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>` 
-    
-}).join('')
+function renderMostRecentScore() {
+
+    var mostRecentScore = JSON.parse(localStorage.getItem("mostRecentScores"));
+
+    var button = document.createElement("button"); button.textContent = "High Scores List!"
+}
+
+
+//see webapi's activities 25 and 26 from class 
